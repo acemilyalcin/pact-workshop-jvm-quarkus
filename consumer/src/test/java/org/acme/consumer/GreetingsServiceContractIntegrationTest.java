e3ef51ccb38e640e59f6ecb6f3a2d47efd0da5a8
acemilyalcin <acemilyalcin@gmail.com>
2023-08-06 22:36:55 +0200

    Test added for end point interaction

--- consumer/src/test/java/org/acme/consumer/GreetingsServiceContractIntegrationTest.java
+++ consumer/src/test/java/org/acme/consumer/GreetingsServiceContractIntegrationTest.java
@@ -1,5 +1,6 @@
 package org.acme.consumer;
 
+import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
@@ -7,11 +8,17 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import io.quarkus.test.junit.QuarkusTest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.gradle.internal.impldep.javax.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 @ExtendWith(PactConsumerTestExt.class)
@@ -29,7 +36,7 @@ class GreetingsServiceContractIntegrationTest {
                 .path("/hello")
                 .method("GET")
                 .willRespondWith()
-                .status(200)
+                .status(200) // if this argument changed with 201 test will fail
                 .body(new PactDslJsonBody()
                         .stringType("greeting")
                 )
@@ -42,5 +49,12 @@ class GreetingsServiceContractIntegrationTest {
         Greeting actualGreeting = greetingService.getGreeting();
         System.out.println(actualGreeting.greeting);
         Assertions.assertNotNull(actualGreeting.greeting);
+    }
+
+    @Test
+    @PactTestFor(providerName = "Provider")
+    void test(MockServer mockServer) throws IOException {
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/hello").execute().returnResponse();
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     }
 }
